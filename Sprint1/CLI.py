0a418376f2093083eeb40568b5b3388226f3cec6
Michael Lam <mlam11@uoguelph.ca>
2022-09-16 19:27:58 -0400

    Implemented filter flag options

--- Sprint1/CLI.py
+++ Sprint1/CLI.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from operator import contains
 
 #to display a singular course
@@ -20,16 +21,62 @@ def readFile(filename):
     courseData = json.load(file)
     return courseData
 
-#do user input for what kind of filtering, and then probabaly call the specific filtering functions and display the entire list w disaplYCourseList
-def filterQuery(courseData):
-    pass
+def displayFilterOpts():
+    print()
+    print("      Filter Options      ")
+    print("-" * 26)
+    print("| \tStatus\t\t |")
+    print("| \tName\t\t |")
+    print("| \tFaculty\t\t |")
+    print("| \tCredits\t\t |")
+    print("| \tLevel\t\t |")
+    print("| \tBack\t\t |")
+    print("| \tExit\t\t |")
+    print("-" * 26)
+    print()
 
+def filterStatus(courseData, status):
+    for course in courseData:
+        if status == course['Status'].lower():
+            displayCourse(course)
+
+def filterFaculty(courseData, faculty):
+    for course in courseData:
+        if faculty in course['Faculty'].lower():
+            displayCourse(course)
+
+def filterName(courseData, name):
+    for course in courseData:
+        if course['Name'].lower().replace("*", "").startswith(name):
+            displayCourse(course)
+
+def filterLevel(courseData, level):
+    for course in courseData:
+        if level == course['Level'].lower():
+            displayCourse(course)
+
+def filterCredits(courseData, numCredits):
+    for course in courseData:
+        if float(numCredits) == float(course['Credits']):
+            displayCourse(course)
+
+def closeCLI():
+    print("*" * 70)
+    print("EXITING...")
+    sys.exit()
 
 courseData = readFile("courses.json")
+userInput = ""
+filterOption = ""
+statusOption = ""
+nameOption = ""
+levelOption = ""
+creditsOption = ""
+facultyOption = ""
 
 print("*" * 70)
 print("Welcome to the command line interface for course searching")
-userInput = ""
+
 while (userInput != "exit"):
     print()
     userInput = input("Select one of the options: search/filter/exit\n").lower()
@@ -37,15 +84,86 @@ while (userInput != "exit"):
         userInput = input("Enter search query:\n").lower()
         print("\n **RESULTS**\n")
         search(courseData, userInput.replace("*", ""))
+    
     elif (userInput == "filter"):
-        filterQuery(courseData)
-    elif (userInput == "exit"):
+
+        while (filterOption != "exit"):
+            displayFilterOpts()
+            filterOption = input("Enter a filter option: ").lower()
+
+            if (filterOption == "status"):
+                while (statusOption != "exit"):
+                    statusOption = input("Enter a status option to filter by (open/closed): ").lower()
+                    if (statusOption == "open" or statusOption == "closed"):
+                        filterStatus(courseData, statusOption)
+                        break
+                    elif (statusOption == "exit"):
+                        closeCLI()
+                    else:
+                        print("Invalid status option\n")
+                        statusOption = ""
+
+            elif (filterOption == "name"):
+                while (nameOption != "exit"):
+                    nameOption = input("Enter a name to filter by (e.g. ACCT*1220): ").lower().replace("*", "")
+                    if (nameOption == "exit"):
+                        closeCLI()
+                    else:
+                        filterName(courseData, nameOption)
+                        break
+
+            elif (filterOption == "faculty"):
+                while (facultyOption != "exit"):
+                    facultyOption = input("Enter a faculty to filter by (e.g. Lassou): ").lower()
+                    if (facultyOption == "exit"):
+                        closeCLI()
+                    else:
+                        filterFaculty(courseData, facultyOption)
+                        break
+
+            elif (filterOption == "credits"):
+                while (creditsOption != "exit"):
+                    creditsOption = input("Enter number of credits to filter by (e.g. 0.50): ")
+                    if (creditsOption == "exit"):
+                        closeCLI()
+                    else:
+                        try:
+                            numCredits = float(creditsOption)
+                            filterCredits(courseData, creditsOption)
+                        except:
+                            print("Invalid credits option\n")
+                            creditsOption = ""
+                        break
+
+            elif (filterOption == "level"):
+                while (levelOption != "exit"):
+                    levelOption = input("Enter a level option (undergraduate/graduate): ").lower()
+                    if (levelOption == "undergraduate" or levelOption == "graduate"):
+                        filterLevel(courseData, levelOption)
+                        break
+                    elif (levelOption == "exit"):
+                        closeCLI()
+                    else:
+                        print("Invalid level option\n")
+                        levelOption = ""
+
+            elif (filterOption == "back"):
+                filterOption = ""
+                break
+
+            elif (filterOption == "exit"):
+                closeCLI()
+
+            else:
+                print("Invalid filter option\n")
+                filterOption = ""
+        
+    elif (userInput == "sort"):
         break
+
+    elif (userInput == "exit"):
+        closeCLI()
+        
     else:
         print("Invalid Command")
 
-
-print("*" * 70)
-print("EXITING...")
-
-        
