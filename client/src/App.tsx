757f3e28f49e427ae19ec62c0cfcb78b782a0431
Michael Lam <mlam11@uoguelph.ca>
2022-10-20 18:51:18 -0400

    Setup routing & created folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,16 @@
-import './App.css';
-import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Pages
+import { Home } from "./pages/home";
 
 const App = () => {
-
-  const submitButton = () => {
-    // TODO: Change endpoint URL
-    axios.get("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => {
-        alert(response.data)
-      });
-  }
-
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#page 2">Page 2</a></li>
-          <li><a href="#page 3">Page 3</a></li>
-        </ul>
-      </nav>
-      <br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br />
-      <h1 className="title">A Simple Website.</h1>
-      <div className="center">
-        <button className="button" type="button" onClick={submitButton}>Test</button>
-      </div>
-      <br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br />
-      <br /><br />
-      <footer>
-        <p className="footerhead">
-          A simple website.<br />For simple people.<br />Team 306
-        </p>
-      </footer>
-    </div >
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import "./Home.css"
+
+const Home: React.FC = () => {
+  const submitButton = () => {
+    // TODO: Change endpoint URL
+    axios.get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        alert(response.data)
+      });
+  }
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#page 2">Page 2</a></li>
+          <li><a href="#page 3">Page 3</a></li>
+        </ul>
+      </nav>
+      <br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br />
+      <h1 className="title">A Simple Website.</h1>
+      <div className="center">
+        <button className="button" type="button" onClick={submitButton}>Test</button>
+      </div>
+      <br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br />
+      <br /><br />
+      <footer>
+        <p className="footerhead">
+          A simple website.<br />For simple people.<br />Team 306
+        </p>
+      </footer>
+    </div >
+  )
+}
+
+export default Home;
