816a08b112c30fdf1640219f84e04c38042f4445
Michael Lam <m.lam08@outlook.com>
2022-11-12 16:39:58 -0500

    eslint error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CoursesType } from "./utils/common_types";
 import { CoursesContext } from "./contexts/course-context";
