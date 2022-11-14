63088916294fcfd8dbedfa1bf115e4a1736958e3
TinsonWang <tinsonwang@gmail.com>
2022-11-13 20:32:42 -0500

    temporarily disabling pylint redefined-outer-name

--- test_server.py
+++ test_server.py
@@ -5,21 +5,25 @@ from client.server import app
 
 @pytest.fixture
 def client():
+    # pylint: disable=redefined-outer-name
     """Function to set up testing client."""
-    with app.test_client() as my_client:
-        yield my_client
+    with app.test_client() as client:
+        yield client
 
-def test_root(my_client):
+def test_root(client):
+    # pylint: disable=redefined-outer-name
     """Function to test root endpoint."""
-    resp = my_client.get('/')
+    resp = client.get('/')
     assert b'Hello World!' in resp.data
 
-def test_course_data(my_client):
+def test_course_data(client):
+    # pylint: disable=redefined-outer-name
     """Function to test /courseData endpoint."""
-    resp = my_client.get('/courseData')
+    resp = client.get('/courseData')
     assert len(resp.get_json()) == 3036
 
-def test_course_search(my_client):
+def test_course_search(client):
+    # pylint: disable=redefined-outer-name
     """Function to test /get-schedule endpoint."""
     headers = {
         "Access-Control-Allow-Origin" :  "*"
@@ -32,5 +36,5 @@ def test_course_search(my_client):
         "course5" : "ZOO*4920*01 (9393) Lab Studies in Ornithology",
     }
 
-    response = my_client.post('/get-schedule', data=data, headers=headers)
+    response = client.post('/get-schedule', data=data, headers=headers)
     assert len(response.get_json()) == 5
